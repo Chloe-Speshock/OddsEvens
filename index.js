e561686493c82bf18af0fb57ef10beef8f0d714c
Chloe Speshock <cespeshock@gmail.com>
2024-03-08 13:25:03 -0600

    added functional buttons that sort numbers

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // TODO: this file! :)
+
+const form = document.querySelector("form");
+const numberBank = document.querySelector("#numberBank");
+const sortOneButton = document.querySelector("#sortOne");
+const sortAllButton = document.querySelector("#sortAll");
+
+const state = {
+  numbers: [],
+  odds: [],
+  evens: [],
+};
+//These are the event listeners
+form.addEventListener("submit", addNumber);
+render();
+sortOneButton.addEventListener("click", sortOne);
+sortAllButton.addEventListener("click", sortAll);
+
+function addNumber(event) {
+  event.preventDefault();
+
+  const number = event.target.number.value;
+
+  state.numbers.push(number);
+  event.target.number.value = "";
+  render();
+}
+function render() {
+  document.querySelector("#numberBank output").textContent =
+    state.numbers.join(", ");
+  document.querySelector("#odds output").textContent = state.odds.join(", ");
+  document.querySelector("#evens output").textContent = state.evens.join(", ");
+}
+function sortOne() {
+  const number = state.numbers.shift();
+  if (number % 2 === 0) {
+    state.evens.push(number);
+  } else {
+    state.odds.push(number);
+  }
+  render();
+}
+function sortAll() {
+  state.numbers.forEach((number) => {
+    if (number % 2 === 0) {
+      state.evens.push(number);
+    } else {
+      state.odds.push(number);
+    }
+  });
+  state.numbers = [];
+
+  render();
+}
